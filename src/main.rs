08def2cb5a2dd7348406214e233c2b1aeeb4c186
Borja González Seoane <garaje@glezseoane.es>
2020-12-30 00:27:44 +0100

    Improve error handling
    A typical error situation is that the TM disk is not connected. Now the
    program prints a checked error and not simply fails.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
 /// Really simple CLI app to obtain the date of the last Mac OS Time Machine backup
 /// e.g. `tmlatestbackup`
 
-use std::process::Command;
+use std::process::{exit, Command};
 
 fn main() {
+    // Call 'tmutil latestbackup'
     let output = Command::new("/usr/bin/tmutil")
         .arg("latestbackup")
         .output()
-        .expect("Error: Failed to execute 'tmutil'.");
+        .expect("Error: Failed to execute 'tmutil latestbackup'.");
 
-    assert!(output.status.success());
-    println!(
-        "{}",
-        String::from_utf8_lossy(&output.stdout)
-            .split("/")
-            .last()
-            .unwrap()
-    );
+    // Check call result
+    if output.status.success() {
+        println!(
+            "{}",
+            String::from_utf8_lossy(&output.stdout)
+                .split("/")
+                .last()
+                .unwrap()
+        );
+        exit(0);
+    } else {
+        println!(
+            "Error: Failed to execute 'tmutil latestbackup'. Is connected the Time Machine disk?"
+        );
+        exit(1);
+    }
 }
